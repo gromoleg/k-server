7ee8623e833f46af885d6c2897d9e9b33c790c4c
Oleg Gromyak <oagromyak@gmail.com>
2016-01-29 12:05:07 +0200

    Add response templates

---
+++ response/templates.py
@@ -0,0 +1,16 @@
+from django.http import JsonResponse
+from django.http import HttpResponse
+
+
+def error_response(error, result):
+    return JsonResponse({"status": 0, "result": result, "error": error})
+
+
+def ok_response(result):
+    return JsonResponse({"status": 1, "result": result, "error": 0})
+
+
+def response(result, error=0):
+    return JsonResponse({"status": bool(error), "result": result, "error": error})
+
+status_ok = ok_response([])
