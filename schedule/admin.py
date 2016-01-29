f2fbf7d4db6028243fe8e23b51d813a2cdf2f641
Oleg Gromyak <oagromyak@gmail.com>
2016-01-29 12:34:33 +0200

    Add Faculty and Group models to django admin

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Faculty, Group
 
-# Register your models here.
+
+@admin.register(Faculty)
+class FacultyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('id', 'faculty_id', 'name')
